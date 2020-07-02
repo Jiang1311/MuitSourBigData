0bf1c00ba229855eecd82d346863e347d8528c8c
jiangjianmin <jiangjianmin@163.com>
2020-07-02 22:18:15 +0800

    init spring app

---
+++ bigdata/src/main/java/com/mutisource/bigdata/BigDataApplication.java
@@ -0,0 +1,16 @@
+package com.mutisource.bigdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Jeremy
+ */
+@SpringBootApplication
+public class BigDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BigDataApplication.class, args);
+    }
+
+}
